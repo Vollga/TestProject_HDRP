d664781013f9fb921d7d3a497041b10c2d77e1a5
Vollga <jancarlpeters@gmail.com>
2019-06-05 01:50:08 +0200

    hm

---
+++ Assets/Scripts/PlayAudioOneshot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOneshot : MonoBehaviour
+{
+    AudioSource Source;
+    AudioClip Soundbyte;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Source = gameObject.GetComponent<AudioSource>();
+        Soundbyte = Source.clip;
+    }
+
+    public void PlayAudioClip()
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(Soundbyte);
+    }
+}
