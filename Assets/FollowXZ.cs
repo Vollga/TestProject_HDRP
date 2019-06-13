7584136ebd1a41b733a0e3f7aaedd1301f322203
Vollga <jancarlpeters@gmail.com>
2019-06-13 18:46:13 +0200

    schinken

---
+++ Assets/FollowXZ.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowXZ : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public bool LookAt = false;
+    public bool Once = false;
+    public float step = 0.1f;
+
+    void Update() {
+        if ( target == null ) return;
+
+        Vector3 tPos = target.position + offset;
+        tPos.y = transform.position.y;
+
+        if ( Once ) {
+            enabled = false;
+            transform.position = Vector3.Lerp(transform.position, tPos, 1f);
+        }
+        else {
+            transform.position = Vector3.Lerp(transform.position, tPos, step);
+        }
+    
+        if ( LookAt ) {
+            transform.LookAt( target );
+        }
+    }
+}
---
+++ Assets/MoveXZ.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveXZ : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Time.deltaTime * 10;
+        transform.Translate(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed, Space.World);
+    }
+}
